ec41a04eb1328eed9313570d3ca50b8302420e15
Sharangan13 <sharangan07@gmail.com>
2024-08-31 13:11:05 +0530

    Add vercel json files

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use('/api/sh/',user)
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
-if(process.env.NODE_ENV ==='production'){
+if(process.env.NODE_ENV ==='development'){
     app.use(express.static(path.join(__dirname,'../frontend/build')))
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'../frontend/build/index.html'))
