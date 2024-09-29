021002df45c31e91990ee174ce99f5c3e425e908
Sharangan13 <sharangan07@gmail.com>
2024-09-29 12:42:21 +0530

    New changes

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -82,7 +82,7 @@ exports.loginUser = catchAsyncError(async(req,res,next)=>{
 
 exports.logoutUser = (req, res, next) => {
     console.log("Logout initiated");
-    res.cookie('token', '', {
+    res.cookie('token',{
         expires: new Date(Date.now() - 1000),
         httpOnly: true,
         // secure: true,
